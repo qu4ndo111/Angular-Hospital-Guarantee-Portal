15b6030caf65b35b7275c3c185e7126791735a55
AnQuan <qu4ndo111@gmail.com>
2026-05-19 19:47:33 +0700

    add returnUrl

--- src/app/core/guard/auth-guard.ts
+++ src/app/core/guard/auth-guard.ts
@@ -6,12 +6,14 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated()) {
+  const isAuthenticated = authService.isAuthenticated();
+
+  const returnUrl = state.url;
+
+  if (isAuthenticated) {
     return true;
   }
 
-  router.navigate(['/auth/login'], {
-    queryParams: { returnUrl: state.url }
-  });
+  router.navigate(['/auth/login'], { queryParams: { returnUrl } });
   return false;
 };
